1367afc75e5cd33d83c6b13775f3a536b754d618
Nanas63 <anais.mulard@gmail.com>
2025-01-24 09:30:09 +0100

    v1

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+$title = "Contact";
+include_once "header.php";?>
+
+
+
+<!-- <a href="index.php">Index</a>
+ -->
+
+
+
+
+<?php include_once "footer.php";?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Page accueil";
+include_once "header.php";?>
+
+
+<!-- <a href="contact.php">Contact</a> -->
+
+
+
+<?php include_once "footer.php";?>
